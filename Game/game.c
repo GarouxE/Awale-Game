b113937ce05a8067d150dff32faee668ba379efa
Garoux Ewan <egaroux@2D-LINUX-020.insa-lyon.fr>
2025-10-20 11:15:43 +0200

    gestion tour du joueur

--- Game/game.c
+++ Game/game.c
@@ -20,11 +20,67 @@ void print_board(Board board) {
     for (int i = 0; i < 6; i++) {
         printf("%d ", board.board[i]);
     }
-    printf("\nplayer2: ");
+    printf("\n");
+    printf("player2: ");
     for (int i = 6; i < 12; i++) {
         printf("%d ", board.board[i]);
     }
+    printf("\n");
 }
+
+void player_turn(Board *board, int player, int place, int direction) {
+    if (player == 1) {
+        place += 6;
+    }
+
+    int pebbles = board->board[place];
+    board->board[place] = 0;
+    int iterator = place;
+
+    // Pebble distribution
+    while (pebbles > 0) {
+        iterator = (iterator + direction + 12) % 12;
+
+        //Do not distribute in the initial position
+        if (iterator == place) {
+            iterator = (iterator + direction + 12) % 12;
+        }
+
+        board->board[iterator] += 1;
+        pebbles--;
+    }
+
+    int opponent_start = (player == 0) ? 6 : 0;
+    int opponent_end   = (player == 0) ? 11 : 5;
+
+    //We capture the oponent stashes containing 2-3 pebbles
+    while (iterator >= opponent_start && iterator <= opponent_end) {
+
+        int count = board->board[iterator];
+        if (count == 2 || count == 3) {
+            board->board[iterator] = 0;
+            iterator = (iterator - direction + 12) % 12;
+        } else {
+            break;
+        }
+    }
+}
+
+int letter_to_int(char letter) {
+    
+    if (letter >= 'A' && letter <= 'Z') {
+        letter = letter + ('a' - 'A');  
+    }
+
+    if (letter >= 'a' && letter <= 'z') {
+        return letter - 'a';
+    }
+
+    // Invalid letter
+    return -1;
+}
+
+
 
 int main() {
     printf("Game lauching...\n");
